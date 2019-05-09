51d0f01c7cedf10ee796564d332177dd099e70c9
David Clews <30339448+webciter@users.noreply.github.com>
2019-05-09 20:54:44 +0100

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,13 @@
+/* Code Edited By David Clews */
+
+/*
+ * min
+ * 
+ * Get the lowest value from an array
+ * 
+ * @param {type} array
+ * @returns {integer}
+ */
+Array.min = function( array ){
+    return Math.min.apply( Math, array );
+};
