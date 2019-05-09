548079b2e392e8daf4ce8bad7a1c5316ea728427
David Clews <30339448+webciter@users.noreply.github.com>
2019-05-09 21:01:38 +0100

    Update Array.js

--- Array.js
+++ Array.js
@@ -5,9 +5,9 @@
  * 
  * Get the lowest value from an array
  * 
- * @param {type} array
+ * @param {array} a THe array to get the lowest value from
  * @returns {integer}
  */
-Array.min = function( array ){
-    return Math.min.apply( Math, array );
+Array.min = function( a ){
+    return Math.min.apply( Math, a );
 };
